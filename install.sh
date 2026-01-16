a7b175be9236946b4d38da3deb9b96ab84233ffe
clyvari <clyvari@users.noreply.github.com>
2026-01-16 22:48:04 +0100

    Allow multiple candidates

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 SCRIPT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd)"
+
+
 
 function log(){
   local lvl="${1}"; shift || true
@@ -24,7 +26,13 @@ function logcnt(){
 if [[ -z "${1:+u}" ]]; then
   info "Installing self in ${INSTALL_DIR}"
   mkdir -p "${INSTALL_DIR}"
-  ln -s -f -T "${SCRIPT_DIR}/install.sh" "${INSTALL_DIR}/app-install"
+  cat <<EOF > "${INSTALL_DIR}/app-install"
+#!/bin/bash
+set -eu
+"${SCRIPT_DIR}/$(basename -- "${BASH_SOURCE[0]}")" "\$@"
+EOF
+  chmod +x "${INSTALL_DIR}/app-install"
+  # ln -s -f -T "${SCRIPT_DIR}/install.sh" "${INSTALL_DIR}/app-install"
   exit 0
 fi
 
@@ -47,6 +55,33 @@ if [[ -f "${APPDIR}/.env" ]]; then
   set +a
 fi
 
+
+function install_pkg(){
+  local app="${1}"; shift
+  local pkgdir="${1}"; shift
+  
+  if [[ -f "${pkgdir}/.install.sh" ]]; then
+    pushd "${pkgdir}" >/dev/null
+    info "Installing using install script"
+    "./.install.sh"
+    popd >/dev/null
+  else
+    if [[ -z "${INSTALL_DIR}" ]]; then
+      error "\$INSTALL_DIR can't be empty"
+      exit 1
+    fi
+    mkdir -p "${INSTALL_DIR}"
+    info "Stowing content of ${app} to ${INSTALL_DIR}"
+    stow . \
+        --dir="${pkgdir}" \
+        --target="${INSTALL_DIR}" \
+        --stow \
+        --ignore='\.((install|condition).sh|env)' \
+        --dotfiles \
+        --adopt
+  fi
+}
+
 pushd "${APPDIR}" >/dev/null
 
 export PATH="${PATH}:${SCRIPTS}"
@@ -54,38 +89,49 @@ export PATH="${PATH}:${SCRIPTS}"
 if [[ ! -z "${MULTI_TARGET:+u}" ]]; then
   info "App \`${APP}\` has multiple candidates"
   for c in *; do
-    if "${c}/.condition.sh"; then
+    if [[ ! -f "${c}/.condition.sh" ]] || "${c}/.condition.sh"; then
       logcnt "Candidate ${c} has been selected"
-      APPDIR="${APPDIR}/${c}"
       CFOUND=y
-      break
+
+      popd >/dev/null
+
+      install_pkg "${APP}" "${APPDIR}/${c}"
+
+      pushd "${APPDIR}" >/dev/null
+
     fi
   done
   if [[ -z "${CFOUND:+u}" ]]; then
     error "No candidate found"
     exit 1
   fi
-fi
-popd >/dev/null
-if [[ -f "${APPDIR}/.install.sh" ]]; then
-  pushd "${APPDIR}" >/dev/null
-  info "Installing using install script"
-  "./.install.sh"
-  popd >/dev/null
 else
-  if [[ -z "${INSTALL_DIR}" ]]; then
-    error "\$INSTALL_DIR can't be empty"
-    exit 1
-  fi
-  mkdir -p "${INSTALL_DIR}"
-  info "Stowing content of ${APP} to ${INSTALL_DIR}"
-  stow . \
-       --dir="${APPDIR}" \
-       --target="${INSTALL_DIR}" \
-       --stow \
-       --ignore='\.((install|condition).sh|env)' \
-       --dotfiles \
-       --adopt
+  install_pkg "${APP}" "${APPDIR}"
 fi
+
+
+
+
+# popd >/dev/null
+# if [[ -f "${APPDIR}/.install.sh" ]]; then
+#   pushd "${APPDIR}" >/dev/null
+#   info "Installing using install script"
+#   "./.install.sh"
+#   popd >/dev/null
+# else
+#   if [[ -z "${INSTALL_DIR}" ]]; then
+#     error "\$INSTALL_DIR can't be empty"
+#     exit 1
+#   fi
+#   mkdir -p "${INSTALL_DIR}"
+#   info "Stowing content of ${APP} to ${INSTALL_DIR}"
+#   stow . \
+#        --dir="${APPDIR}" \
+#        --target="${INSTALL_DIR}" \
+#        --stow \
+#        --ignore='\.((install|condition).sh|env)' \
+#        --dotfiles \
+#        --adopt
+# fi
 
 info "DONE"
